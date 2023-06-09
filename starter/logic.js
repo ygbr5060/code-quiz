3fd93895b5c6fea7569b12da518fda93483c1863
ygbr5060 <aqibkhan5060@gmail.com>
2023-06-09 18:07:24 +0100

    js files with updated code

---
+++ starter/logic.js
@@ -0,0 +1,110 @@
+// Variables
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+// Elements
+var startScreenEl = document.getElementById("start-screen");
+var questionsEl = document.getElementById("questions");
+var questionTitleEl = document.getElementById("question-title");
+var choicesEl = document.getElementById("choices");
+var endScreenEl = document.getElementById("end-screen");
+var finalScoreEl = document.getElementById("final-score");
+var initialsInputEl = document.getElementById("initials");
+var submitBtnEl = document.getElementById("submit");
+var feedbackEl = document.getElementById("feedback");
+
+// Start the quiz
+function startQuiz() {
+  startScreenEl.classList.add("hide");
+  questionsEl.classList.remove("hide");
+
+  // Start the timer
+  timerId = setInterval(function () {
+    time--;
+    document.getElementById("time").textContent = time;
+
+    if (time <= 0) {
+      endQuiz();
+    }
+  }, 1000);
+
+  // Display the first question
+  displayQuestion();
+}
+
+// Display a question
+function displayQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  questionTitleEl.textContent = currentQuestion.question;
+  choicesEl.innerHTML = "";
+
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var choice = currentQuestion.choices[i];
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = choice;
+    choiceBtn.setAttribute("data-index", i);
+    choiceBtn.addEventListener("click", handleAnswerClick);
+    choicesEl.appendChild(choiceBtn);
+  }
+}
+
+// Handle answer click
+function handleAnswerClick(event) {
+  var selectedAnswerIndex = parseInt(event.target.getAttribute("data-index"));
+
+  if (selectedAnswerIndex === questions[currentQuestionIndex].answer) {
+    feedbackEl.textContent = "Correct!";
+  } else {
+    feedbackEl.textContent = "Wrong!";
+    time -= 10;
+    if (time < 0) {
+      time = 0;
+    }
+    document.getElementById("time").textContent = time;
+  }
+
+  feedbackEl.classList.remove("hide");
+  setTimeout(function () {
+    feedbackEl.classList.add("hide");
+  }, 1000);
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    endQuiz();
+  } else {
+    displayQuestion();
+  }
+}
+
+// End the quiz
+function endQuiz() {
+  clearInterval(timerId);
+  questionsEl.classList.add("hide");
+  endScreenEl.classList.remove("hide");
+  finalScoreEl.textContent = time;
+}
+
+// Handle submit button click
+function handleSubmitClick() {
+  var initials = initialsInputEl.value.trim();
+
+  if (initials !== "") {
+    // Save the score and initials
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+    var newScore = {
+      initials: initials,
+      score: time
+    };
+    highscores.push(newScore);
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // Redirect to highscores page
+    window.location.href = "highscores.html";
+  }
+}
+
+// Attach event listeners
+document.getElementById("start").addEventListener("click", startQuiz);
+submitBtnEl.addEventListener("click", handleSubmitClick);
---
+++ starter/questions.js
@@ -0,0 +1,43 @@
+// Define your quiz questions and answers here
+var questions = [
+    {
+      question: "What is HTML used for?",
+      choices: ["Styling web pages", "Adding interactivity", "Structuring web content", "Performing calculations"],
+      answer: 2
+    },
+    {
+      question: "What is the main purpose of CSS?",
+      choices: ["Managing server-side code", "Handling database operations", "Styling web pages", "Executing client-side scripts"],
+      answer: 2
+    },
+    {
+      question: "Which programming language is used for web development?",
+      choices: ["JavaScript", "Java", "C++", "Python"],
+      answer: 0
+    },
+    {
+      question: "Which of the following is NOT a valid JavaScript data type?",
+      choices: ["String", "Number", "Boolean", "Function"],
+      answer: 3
+    },
+    {
+      question: "Which programming language is often used for backend web development?",
+      choices: ["Java", "HTML", "CSS", "JavaScript"],
+      answer: 0
+    },
+    {
+      question: "What does API stand for?",
+      choices: ["Application Programming Interface", "Advanced Programming Interface", "Application Process Integration", "Advanced Process Integration"],
+      answer: 0
+    },
+    {
+      question: "What is the purpose of a for loop in programming?",
+      choices: ["To declare variables", "To perform a specific task", "To make decisions", "To repeat a block of code"],
+      answer: 3
+    },
+    // Add more questions here
+  ];
+  
+  // Export the questions array
+  export default questions;
+  
